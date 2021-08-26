384676855a71ac8f3cf51ad27f780949f473accd
7cn95 <za3jhm.38@gmail.com>
2021-08-26 19:18:51 +0300

    v2 update

--- js/app.js
+++ js/app.js
@@ -4,39 +4,97 @@ const recover = document.getElementById('recover');
 const activeCases = document.getElementById('active');
 const death = document.getElementById('deaths');
 const topCountry = document.getElementById('topCountry');
+const closeNoti = document.getElementById('closeNoti');
+const themeBTN = document.getElementById('theme');
 ///////////////////////
-fetch("https://api.covid19api.com/summary")
-    .then(res => res.json())
-    .then(json => {
-        const{TotalConfirmed,TotalRecovered,TotalDeaths}=json.Global;
-    
-        allCases.innerHTML=formatNumber(TotalConfirmed) ;
-        recover.innerHTML=formatNumber(TotalRecovered) ;
-        death.innerHTML=formatNumber(TotalDeaths) ;
-        activeCases.innerHTML=formatNumber(TotalConfirmed-TotalRecovered-TotalDeaths) ;
-    }
-);
-/////////////////////////////
-fetch("https://covid19-api.org/api/status")
-  .then(res => res.json())
-  .then(result => {
-      for (let i = 0; i <= 23; i++) {
-          const {country,cases,recovered,deaths}=result[i];
-          if (country!=="IL") {
-              var contentTag=`<div class="countryList" >
-          <div class="tt"><span class="flag-icon flag-icon-${country.toLowerCase()}"></span>
-          <p>${country}</p></div>
-
-          <div class="t"><p>${formatNumber(cases) }</p>
-          <p>${formatNumber(recovered)}</p>
-          <p>${formatNumber(cases-recovered-deaths)}</p>
-          <p>${formatNumber(deaths)}</p>
-          </div></div>`
-         topCountry.innerHTML+=contentTag;
-          }
+setInterval(() => {
+        if (note.style.display == "none") {
+            note.style.display = "flex"
         }
+    }, 60000)
+    // ! display notification alarte
+function noti() {
+    note.style.display = "none"
+}
+// ! convert btw light and dark theme
+function dark() {
+    if (document.body.className == "light") {
+        document.body.classList.replace("light", "dark")
+        themeBTN.innerText = "Light"
+        themeBTN.style.color = "#F3F3F3"
+    } else {
+        document.body.classList.replace("dark", "light")
+        themeBTN.innerText = "Dark"
+        themeBTN.style.color = "#000"
     }
-);
-function formatNumber (num) {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+}
+////////////////////// ? Events
+closeNoti.addEventListener("click", noti);
+themeBTN.addEventListener("click", dark)
+    ////////////////////////
+    //? get data as a Promis from api
+    //! corona data and country data
+async function getData() {
+    try {
+        const res2 = await fetch("https://api.covid19api.com/summary");
+        const resSummary = await res2.json();
+        const restCoun = await fetch("https://restcountries.eu/rest/v2/lang/ar");
+        const restArabicCountry = await restCoun.json();
+        return { resSummary, restArabicCountry }
+    } catch (err) {
+        console.log(err + "Summary error");
+    }
+}
+
+// display this data to main section in page
+getData()
+    .then((data) => {
+        let dataGlobal = data.resSummary.Global;
+
+        allCases.innerHTML = formatNumber(dataGlobal.TotalConfirmed);
+        recover.innerHTML = formatNumber(dataGlobal.TotalRecovered);
+        death.innerHTML = formatNumber(dataGlobal.TotalDeaths);
+        //activeCases.innerHTML = formatNumber(dataGlobal.TotalConfirmed - dataGlobal.TotalRecovered - dataGlobal.TotalDeaths);
+    }).catch((err) => {
+        console.log(err);
+    })
+let notArabicCountry = ["TD"] // todo convert nested if down below to array
+getData().then((data) => {
+        let dataCoun = data.resSummary.Countries;
+        let araCountries = data.restArabicCountry;
+
+        araCountries.forEach(araCountry => {
+            const { alpha2Code, flag, name } = araCountry;
+            dataCoun.forEach(dataco => {
+                const { Country, TotalConfirmed, TotalRecovered, TotalDeaths, CountryCode } = dataco;
+                if (CountryCode == alpha2Code) {
+                    if (alpha2Code != "TD")
+                        if (alpha2Code != "IL")
+                            if (alpha2Code != "KM")
+                                if (alpha2Code != "DJ")
+                                    if (alpha2Code != "ER") {
+                                        var contentTag = `
+                                        <div class="countryList" >
+                                        <div>
+                                        <img src="${flag}" alt="flag" style="width: 150px;">
+                                        <p>${name}</p></div>
+
+                                        <div class="t"><p>${formatNumber(TotalConfirmed) }</p>
+                                        <p>${formatNumber(TotalRecovered)}</p>
+                                        <p>${formatNumber(TotalConfirmed-TotalRecovered-TotalDeaths)}</p>
+                                        <p>${formatNumber(TotalDeaths)}</p>
+                                        </div>
+                                        </div>`
+                                        topCountry.innerHTML += contentTag;
+                                    }
+                }
+            });
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")
 }
